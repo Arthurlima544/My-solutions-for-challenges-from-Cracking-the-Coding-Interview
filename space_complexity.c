5aeb5187205d76e3cc9cc3cdc45425ea42946ad0
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-01-06 19:00:33 -0300

    comments space complexity

--- space_complexity.c
+++ space_complexity.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+/*
+    when we call for sum_with_recursion(5) the call stack:
+    - sum_with_recursion(5)
+        - sum_with_recursion(4)
+            - sum_with_recursion(3)
+                - sum_with_recursion(2)
+                    - sum_with_recursion(1)
+                        - sum_with_recursion(0)
+    this is added to the recursion stack and it takes up actual memory, the compiler
+    could identify this thing and optimize converting into an for loop as in fuction 
+    sum_with_for(), but sometimes we need some tail recursion to optimise it.
+*/
+
+// Uses O(n) on space complexity, we use 'n' memory for each call in stack.
 int sum_with_recursion(int number){
     if(number <= 0){
         return 0;
@@ -10,7 +24,7 @@ int sum_with_recursion(int number){
 int pairSum(int a, int b){
     return a + b;
 }
-
+// Uses O(1) on space complexity, we only use sum and i, in the memory
 int sum_with_for(int number){
     int sum = 0;
     for(int i = 1; i <= number ; i++){
